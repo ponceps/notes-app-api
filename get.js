a04fbd59df82197b8e746e81f893916fa1a750c1
Lucas Ponce <lucas.ponce@iclinic.com.br>
2019-10-07 23:19:35 -0300

    Changes needed after tests with mock

--- get.js
+++ get.js
@@ -1,18 +1,17 @@
 import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
-import { fail } from 'assert';
 
 export async function main(event, context) {
     const params = {
         TableName: "notes",
         Key: {
-            userId: event.requestContext.identity.congnitoIdentityId,
+            userId: event.requestContext.identity.cognitoIdentityId,
             noteId: event.pathParameters.id
         }
     };
 
     try {
-        const result = await dynamoDbLib("get", params);
+        const result = await dynamoDbLib.call("get", params);
         if (result.Item) {
             return success(result.Item);
         } else {
